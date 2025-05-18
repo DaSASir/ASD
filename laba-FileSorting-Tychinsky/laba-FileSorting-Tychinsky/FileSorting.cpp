b7ebf8dac3d969b2e7a4ba82aa64e62b5f98ab4c
DaSASir <Sapukov1701@mail.ru>
2025-05-18 17:46:35 +0700

    написаны все требуемые функции для реализации программы

--- laba-FileSorting-Tychinsky/laba-FileSorting-Tychinsky/FileSorting.cpp
+++ laba-FileSorting-Tychinsky/laba-FileSorting-Tychinsky/FileSorting.cpp
@@ -5,16 +5,34 @@
 
 //требуемые функции
 bool createFileWithRandomNumbers(const std::string& fileName, const int numbersCount, const int maxNumberValue) {
+    if (numbersCount <= 0)
+        return false;
 
+    std::ofstream file(fileName);
+    if (!file.is_open())
+        return false;
 
+    for (int i = 0; i < numbersCount; ++i)
+        file << rand() % (maxNumberValue - 0 + 1) + maxNumberValue << " ";
 
-    return false;
+    return true;
 }
 
 bool isFileContainsSortedArray(const std::string& fileName) {
+    std::ifstream file(fileName);
+    if (!file.is_open())
+        return false;
 
+    int value1, value2;
+    file >> value1;
+    while (file >> value2) {
+        if (value1 > value2)
+            return false;
 
-    return false;
+        value1 = value2;
+    }
+
+    return true;
 }
 
 int createAndSortFile(const std::string& fileName, const int numbersCount, const int maxNumberValue) {
